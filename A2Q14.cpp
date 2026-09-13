593cf8b06ed7d236cfc08c972b119a92baad8475
pradhyumna <2008pradhyumna@gmail.com>
2026-09-13 19:45:14 +0530

    Update A2Q14.cpp

--- A2Q14.cpp
+++ A2Q14.cpp
@@ -1,1 +1,21 @@
-#include
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cout << "enter 3 numbers: ";
+	cin >> a >> b >> c;
+	if(a >= b) {
+	    if(a >= c) cout << a;
+	    else cout << c;
+	}
+	else {
+	    if(b >= c) cout << b;
+	    else cout << c;
+	}
+	
+	cout << " is the largest among the entered numbers";
+	
+	return 0;
+}
