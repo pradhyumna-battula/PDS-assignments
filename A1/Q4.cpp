c0e8a75d6e47062a40ca351006fbb95ddb4b484a
pradhyumna <2008pradhyumna@gmail.com>
2026-09-14 15:41:29 +0530

    Create Q4.cpp

---
+++ A1/Q4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Size of char\t\t: " << sizeof(char) << " byte(s)\n";
+    cout << "Size of short\t\t: " << sizeof(short) << " byte(s)\n";
+    cout << "Size of int\t\t: " << sizeof(int) << " byte(s)\n";
+    cout << "Size of long\t\t: " << sizeof(long) << " byte(s)\n";
+    cout << "Size of long long\t: " << sizeof(long long) << " byte(s)\n";
+    cout << "Size of float\t\t: " << sizeof(float) << " byte(s)\n";
+    cout << "Size of double\t\t: " << sizeof(double) << " byte(s)\n";
+    cout << "Size of long double\t: " << sizeof(long double) << " byte(s)\n";
+
+    return 0;
+}
