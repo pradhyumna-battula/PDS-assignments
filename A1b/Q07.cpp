8d3769cbf83a0dbc576aae8262bc90310bd10946
pradhyumna <2008pradhyumna@gmail.com>
2026-09-20 18:50:52 +0530

    Update Q07.cpp

--- A1b/Q07.cpp
+++ A1b/Q07.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
-#include <typeinfo>
 
 using namespace std;
 
 int main() {
-    auto a = 5;
-    auto b = 5.5;
-    auto c = 'x';
-    auto d = "hello";
-    auto e = true;
-    cout << "a is type: " << typeid(a).name() << endl;
-    cout << "b is type: " << typeid(b).name() << endl;
-    cout << "c is type: " << typeid(c).name() << endl;
-    cout << "d is type: " << typeid(e).name() << endl;
+    unsigned int a = -1;
+    unsigned int b = 0;
+
+    cout << a << endl;
+    cout << b - 1;
 
     return 0;
 }
