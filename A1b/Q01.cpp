0e69819a910770cbe708b62506507a999e1f5d48
pradhyumna <2008pradhyumna@gmail.com>
2026-09-20 16:10:42 +0530

    Create Q01.cpp

---
+++ A1b/Q01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myInt;
+    float myFloat;
+    double myDouble;
+    char myChar;
+    bool myBool;
+    long myLong;
+    long long myLongLong;
+
+    cout << "=== Size Explorer ===\n";
+    cout << "Size of int: " << sizeof(myInt) << " bytes\n";
+    cout << "Size of float: " << sizeof(myFloat) << " bytes\n";
+    cout << "Size of double: " << sizeof(myDouble) << " bytes\n";
+    cout << "Size of char: " << sizeof(myChar) << " bytes\n";
+    cout << "Size of bool: " << sizeof(myBool) << " bytes\n";
+    cout << "Size of long: " << sizeof(myLong) << " bytes\n";
+    cout << "Size of long long: " << sizeof(myLongLong) << " bytes\n";
+
+    return 0;
+}
