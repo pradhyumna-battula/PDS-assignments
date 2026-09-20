97302498d5cac50b1c79f91fdebd62084a6919cb
pradhyumna <2008pradhyumna@gmail.com>
2026-09-20 17:54:04 +0530

    Create Q06.cpp

---
+++ A1b/Q06.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char char1;
+    cout << "enter a character: ";
+    cin >> char1;
+    
+    cout << "the character ASCII value: " << int(char1);
+
+    int int1;
+    cout << "\nenter an ASCII value (0 - 127): ";
+    cin >> int1;
+
+    cout << "the corrosponding char: " << char(int1);
+
+
+    return 0;
+}
